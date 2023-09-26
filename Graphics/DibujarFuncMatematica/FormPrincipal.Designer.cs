af4d3580029c4266545514bc445d061b17a16d18
fernandofilipuzzi.utn <fernandofilipuzzi-utn>
2023-09-26 12:26:40 -0300

    iniciando repo

---
+++ Graphics/DibujarFuncMatematica/FormPrincipal.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace DibujarFuncMatematica
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(396, 230);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(414, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Guardar Imagen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnGuardarImagen_Click);
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 251);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormPrincipal";
+            this.Text = "Form Principal";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Graphics/DibujarFuncMatematica/FormPrincipal.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DibujarFuncMatematica
+{
+   
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            DibujarEjes(g, pictureBox1.Width, pictureBox1.Height);
+            DibujarFuncion(g, pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void DibujarEjes(Graphics g, int ancho, int alto)
+        {
+            #region pinto el fondo
+            Brush brush = new SolidBrush(Color.White);
+            g.FillRectangle(brush, new RectangleF(0, 0, ancho, alto));
+            #endregion
+
+            #region dibujar eje x e y 
+            Pen pen = new Pen(Color.Gray);
+            int posXEje = ancho / 2;
+            int posYEje = alto / 2;
+
+            g.DrawLine(pen, new Point(0, posYEje), new Point(ancho - 1, posYEje));
+            g.DrawLine(pen, new Point(posXEje, 0), new Point(posXEje, alto));
+            #endregion
+        }
+
+        private void DibujarFuncion(Graphics g, int ancho, int alto)
+        {
+            Pen pen = new Pen(Color.Blue);
+
+            int posXEje = ancho / 2;
+            int posYEje = alto / 2;
+            int Amplitud = 5;
+
+            //necesito el punto evaluado en el borde como posición inicial
+            int x = 0;
+            int xp0 = x - posXEje; 
+            int yp0 = (int)(Amplitud * Math.Sin(2 * Math.PI / 3 * xp0));
+
+            for (x = 2; x < ancho; x+=3) 
+            {
+                int xp1 = x - posXEje;
+
+                //calculo el siguiente punto, 
+                int yp1=(int)(5*Amplitud*Math.Sin(2*Math.PI/20*xp1));
+                
+                #region dibujo 
+                g.DrawLine(pen, new Point(xp0+ posXEje, -yp0+ posYEje), new Point(xp1+ posXEje, -yp1 + posYEje));
+                #endregion
+
+                //guardo la posición anterior
+                xp0 = xp1;
+                yp0 = yp1;
+            }
+        }
+
+        private void btnGuardarImagen_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "jpg|*.jpg";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                
+                Bitmap bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics g = Graphics.FromImage(bitmap);
+                DibujarEjes(g, pictureBox1.Width, pictureBox1.Height);
+                DibujarFuncion(g, pictureBox1.Width, pictureBox1.Height);
+                bitmap.Save(saveFileDialog1.FileName, ImageFormat.Jpeg);
+                g.Dispose();
+                bitmap.Dispose();
+            }
+        }
+    }
+}
